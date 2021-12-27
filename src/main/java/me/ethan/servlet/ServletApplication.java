6fb8fed613b28e721fe1c549df92acb19a97f59b
EthanKim <hey0507@gmail.com>
2021-12-27 15:48:57 +0900

    feat : 새 프로젝트 시작

---
+++ src/main/java/me/ethan/servlet/ServletApplication.java
@@ -0,0 +1,13 @@
+package me.ethan.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+	}
+
+}
