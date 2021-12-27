1b9eee2e716e08e2c1d6e56c5e8943a6bc6ed3b5
EthanKim <hey0507@gmail.com>
2021-12-28 02:53:13 +0900

    docs : GET 파라미터 처리 - HTML form (메시지바디) 로 넘기기   - request.getParameter() 를 통한 조회 - API messageBody, JSON   - text/plain 을 통한 전송   - Jackson 을 이용해 JSON 을 받아오기

---
+++ src/main/java/me/ethan/servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package me.ethan.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
---
+++ src/main/java/me/ethan/servlet/basic/requset/RequestBodyStringServlet.java
@@ -0,0 +1,25 @@
+package me.ethan.servlet.basic.requset;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "RequestBodyStringServlet" , urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ServletInputStream inputStream = request.getInputStream(); // request의 바이트코드를 받아옴
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);//바이트와 문자 간의 변화가 있을 땐 문자 정보가 꼭 필요하다.
+
+        System.out.println("messageBody = " + messageBody);
+        response.getWriter().write("OK!");
+    }
+}
